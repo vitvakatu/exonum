4490c9ec097e763091e2667a5ff8f06395d34eff
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-06-28 13:55:50 +0300

    Basic proc macro

--- exonum-storage-derive/src/lib.rs
+++ exonum-storage-derive/src/lib.rs
@@ -1,7 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+extern crate proc_macro2;
+extern crate syn;
+#[macro_use]
+extern crate quote;
+extern crate exonum;
+
+use proc_macro2::TokenStream;
+
+#[proc_macro_derive(Schema)]
+pub fn schema_custom_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input: TokenStream = input.into();
+
+    let output: TokenStream = input;
+
+    output.into()
 }
