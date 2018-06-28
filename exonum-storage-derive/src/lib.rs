ccafa90ccb3cfd78e2e13da5d9966ce991a1c62c
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-06-28 23:50:02 +0300

    Massive refactoring

--- exonum-storage-derive/src/lib.rs
+++ exonum-storage-derive/src/lib.rs
@@ -8,8 +8,106 @@ extern crate quote;
 extern crate exonum;
 use exonum::storage::MapIndex;
 
-
 use proc_macro2::TokenStream;
+
+#[derive(Clone, Debug, Copy)]
+enum IndexMutability {
+    Mutable,
+    Immutable,
+}
+
+impl IndexMutability {
+    pub fn into_token_stream(self) -> TokenStream {
+        match self {
+            IndexMutability::Immutable => quote!(&'a ::exonum::storage::Snapshot),
+            IndexMutability::Mutable => quote!(&'a mut ::exonum::storage::Fork),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Copy)]
+enum IndexTypeParamsCount {
+    Zero,
+    One,
+    Two,
+}
+
+impl From<IndexType> for IndexTypeParamsCount {
+    fn from(index_type: IndexType) -> Self {
+        match index_type {
+            IndexType::Map => IndexTypeParamsCount::Two,
+            IndexType::List => IndexTypeParamsCount::One,
+            IndexType::ProofMap => IndexTypeParamsCount::Two,
+            IndexType::ProofList => IndexTypeParamsCount::One,
+            IndexType::Entry => IndexTypeParamsCount::Zero,
+            IndexType::SparseList => IndexTypeParamsCount::One,
+            IndexType::KeySet => IndexTypeParamsCount::One,
+            IndexType::ValueSet => IndexTypeParamsCount::One,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Copy)]
+enum IndexType {
+    Map,
+    List,
+    ProofMap,
+    ProofList,
+    Entry,
+    SparseList,
+    KeySet,
+    ValueSet,
+}
+
+impl From<syn::Ident> for IndexType {
+    fn from(ident: syn::Ident) -> Self {
+        match ident {
+            ref x if x == "MapIndex" => IndexType::Map,
+            ref x if x == "MapIndex" => IndexType::List,
+            ref x if x == "MapIndex" => IndexType::ProofMap,
+            ref x if x == "MapIndex" => IndexType::ProofList,
+            ref x if x == "MapIndex" => IndexType::Entry,
+            ref x if x == "MapIndex" => IndexType::SparseList,
+            ref x if x == "KeySet" => IndexType::KeySet,
+            ref x if x == "ValueSet" => IndexType::ValueSet,
+            _ => panic!("Invalid index type"),
+        }
+    }
+}
+
+impl IndexType {
+    fn into_token_stream(self, mutability: IndexMutability, generic_params: &syn::PathSegment) -> TokenStream {
+        let index_type = match self {
+            IndexType::Map => quote!(::exonum::storage::MapIndex),
+            List => quote!(::exonum::storage::ListIndex),
+            ProofMap => quote!(::exonum::storage::ProofMapIndex),
+            ProofList => quote!(::exonum::storage::ProofListIndex),
+            Entry => quote!(::exonum::storage::Entry),
+            SparseList => quote!(::exonum::storage::SparseListIndex),
+            KeySet => quote!(::exonum::storage::KeySetIndex),
+            ValueSet => quote!(::exonum::storage::ValueSetIndex),
+        };
+
+        let view_type = mutability.into_token_stream();
+
+        type_with_parameters(index_type, view_type, self.into(), generic_params)
+    }
+}
+
+fn type_with_parameters(ty: TokenStream, first_param: TokenStream, param_count: IndexTypeParamsCount, generic_params: &syn::PathSegment) -> TokenStream {
+    use IndexTypeParamsCount::*;
+    match param_count {
+        Zero => quote!(#ty <#first_param>),
+        One => {
+            let param = get_type_parameter(generic_params).unwrap();
+            quote!(#ty <#first_param, #param>)
+        },
+        Two => {
+            let (second, third) = get_type_parameters(generic_params).unwrap();
+            quote!(#ty <#first_param, #second, #third>)
+        },
+    }
+}
 
 #[proc_macro_derive(Schema, attributes(schema))]
 pub fn schema_custom_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -60,79 +158,62 @@ fn generate_functions(ast: &syn::DeriveInput) -> TokenStream {
                                 panic!("Error");
                             }
                         }
-                        let index_type = match index_type {
+                        let index_type_raw = match index_type {
                             Some(x) => x,
                             None => continue,
                         };
+                        let index_type: IndexType = index_type_raw.clone().into();
                         if let syn::Type::Path(ref type_path) = field_type {
                             let last_segment = type_path.path.segments.last().map(|v| v.into_value()).clone().unwrap();
-                            let index_type_name = last_segment.ident.clone();
-                            let (key_type, value_type) = match get_type_parameters(last_segment) {
-                                Some(v) => v,
-                                None => continue,
-                            };
-                            let struct_definition = quote!{
-                                pub struct #index_type_name <T, K, V> {
-                                    _i: ::std::marker::PhantomData<T>,
-                                    _k: ::std::marker::PhantomData<K>,
-                                    _v: ::std::marker::PhantomData<V>,
-                                }
-                            };
-                            let impl_new = quote!{
-                                impl<T, K, V> #index_type_name <T, K, V> {
-                                    pub fn new() -> Self {
-                                        Self {
-                                            _i: Default::default(),
-                                            _k: Default::default(),
-                                            _v: Default::default(),
-                                        }
-                                    }
-                                }
-                            };
+                            let user_index_type_name = last_segment.ident.clone();
 
-                            let read_func_ident = ident.to_string() + "_read";
-                            let read_func_ident = syn::Ident::new(&read_func_ident, proc_macro2::Span::call_site());
+                            let index_type_token_stream = index_type.into_token_stream(IndexMutability::Immutable, &last_segment);
+
+                            tokens.extend(index_type_definition(&user_index_type_name));
+
+                            let read_func_ident = func_ident(ident.clone(), IndexMutability::Immutable);
+                            let write_func_ident = func_ident(ident.clone(), IndexMutability::Mutable);
+
+                            let user_index_type_token_stream = type_with_parameters(quote!(#user_index_type_name), quote!(T), index_type.into(), &last_segment);
+
 
                             let impl_read = quote!{
-                                impl<'a, T: AsRef<::exonum::storage::Snapshot>> #index_type_name <T, #key_type, #value_type> {
-                                    pub fn read(&self, view: &'a ::exonum::storage::Snapshot) -> #index_type<&'a ::exonum::storage::Snapshot, #key_type, #value_type> {
-                                        #index_type::new(stringify!(ident), view)
+                                impl<'a, T: AsRef<::exonum::storage::Snapshot>> #user_index_type_token_stream {
+                                    pub fn read(&self, view: &'a ::exonum::storage::Snapshot) -> #index_type_token_stream {
+                                        #index_type_raw::new(stringify!(ident), view)
                                     }
                                 }
 
-                                impl<T: AsRef<::exonum::storage::Snapshot>> #struct_ident <T> {
-                                    pub fn #read_func_ident(&self) -> #index_type<& ::exonum::storage::Snapshot, #key_type, #value_type> {
+                                impl<'a, T: AsRef<::exonum::storage::Snapshot>> #struct_ident <T> {
+                                    pub fn #read_func_ident(&'a self) -> #index_type_token_stream {
                                         self.#ident.read(self.view.as_ref())
                                     }
                                 }
                             };
+                            tokens.extend(impl_read);
 
-                            let write_func_ident = ident.to_string() + "_write";
-                            let write_func_ident = syn::Ident::new(&write_func_ident, proc_macro2::Span::call_site());
+                            let index_type_token_stream = index_type.into_token_stream(IndexMutability::Mutable, &last_segment);
+
+                            let user_index_type_token_stream = type_with_parameters(quote!(#user_index_type_name), quote!(&'a mut ::exonum::storage::Fork), index_type.into(), &last_segment);
 
                             let impl_write = quote!{
-                                impl<'a> #index_type_name <&'a mut ::exonum::storage::Fork, #key_type, #value_type> {
-                                    pub fn write(&self, view: &'a mut ::exonum::storage::Fork) -> #index_type<&'a mut ::exonum::storage::Fork, #key_type, #value_type> {
-                                        #index_type::new(stringify!(ident), view)
+                                impl<'a> #user_index_type_token_stream {
+                                    pub fn write(&self, view: &'a mut ::exonum::storage::Fork) -> #index_type_token_stream {
+                                        #index_type_raw::new(stringify!(ident), view)
                                     }
                                 }
 
                                 impl<'a> #struct_ident <&'a mut ::exonum::storage::Fork> {
-                                    pub fn #write_func_ident(&'a mut self) -> #index_type<&'a mut ::exonum::storage::Fork, #key_type, #value_type> {
-                                        #index_type::new(stringify!(ident), self.view)
+                                    pub fn #write_func_ident(&'a mut self) -> #index_type_token_stream {
+                                        self.#ident.write(self.view)
                                     }
                                 }
                             };
+                            tokens.extend(impl_write);
 
-                            tokens.extend(quote! {
-                                #struct_definition
-                                #impl_new
-                                #impl_read
-                                #impl_write
-                            });
                             struct_fields.push_value(
                                 quote!(#ident : {
-                                    let index: #field_type = #index_type_name::new();
+                                    let index: #field_type = #user_index_type_name::new();
                                     index
                                 })
                             );
@@ -159,6 +240,26 @@ fn generate_functions(ast: &syn::DeriveInput) -> TokenStream {
     }
 }
 
+fn index_type_definition(index_type_name: &syn::Ident) -> TokenStream {
+    quote! {
+        pub struct #index_type_name <T, K, V> {
+            _i: ::std::marker::PhantomData<T>,
+            _k: ::std::marker::PhantomData<K>,
+            _v: ::std::marker::PhantomData<V>,
+        }
+
+        impl<T, K, V> #index_type_name <T, K, V> {
+            pub fn new() -> Self {
+                Self {
+                    _i: Default::default(),
+                    _k: Default::default(),
+                    _v: Default::default(),
+                }
+            }
+        }
+    }
+}
+
 fn get_type_parameters(last_segment: &syn::PathSegment) -> Option<(syn::GenericArgument, syn::GenericArgument)> {
     let types = if let syn::PathArguments::AngleBracketed(ref args) = last_segment.arguments {
         let mut args = args.args.iter();
@@ -170,5 +271,26 @@ fn get_type_parameters(last_segment: &syn::PathSegment) -> Option<(syn::GenericA
         None
     };
     types
+}
+
+fn get_type_parameter(last_segment: &syn::PathSegment) -> Option<syn::GenericArgument> {
+    let type_parameter = if let syn::PathArguments::AngleBracketed(ref args) = last_segment.arguments {
+        let mut args = args.args.iter();
+        let _ = args.next();
+        let value = args.next().unwrap();
+        Some(value.clone())
+    } else {
+        None
+    };
+    type_parameter
+}
+
+fn func_ident(ident: syn::Ident, mutability: IndexMutability) -> syn::Ident {
+    let postfix = match mutability {
+        IndexMutability::Immutable => "_read",
+        IndexMutability::Mutable => "_write",
+    };
+    let read_func_ident = ident.to_string() + postfix;
+    syn::Ident::new(&read_func_ident, proc_macro2::Span::call_site())
 }
 
